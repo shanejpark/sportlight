1e2b79ab5cf4bb568869f80f5240847dbf0a10bc
Shane Park <shanejpark@gmail.com>
2023-12-03 16:23:14 -0500

    more stuff

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,29 +5,28 @@ import HomePeople from "../components/home-people";
 import HomeCheckboxes from "../components/home-checkboxes";
 
 export default function Main() {
-    return (
-        <div>
-            <div className="row d-flex ms-5 me-5 mt-5">
-                <div className="col-2 d-flex align-items-start mt-3">
-                    {/* <Image src="https://placehold.co/300x200" alt="flag" thumbnail />
+  return (
+    <div>
+      <div className="row d-flex ms-5 me-5 mt-5">
+        <div className="col-2 d-flex align-items-start mt-3">
+          {/* <Image src="https://placehold.co/300x200" alt="flag" thumbnail />
                     <div className='ms-4'>
                         <p>Country</p>
                         <p>Stage</p>
                         <p>Week</p>
                         <p>Date</p>
                     </div> */}
-                    <HomeCheckboxes />
-                </div>
-
-                <div className="col-10 d-flex align-items-center">
-                    <div className="container">
-                    <HomeHighlights />
-                    <HomeMatches />
-                    <HomePeople/>
-                    </div>
-                </div>
-            </div>
+          {/* <HomeCheckboxes /> */}
         </div>
-    )
 
+        <div className="col-10 d-flex align-items-center">
+          <div className="container">
+            <HomeHighlights />
+            <HomeMatches />
+            <HomePeople />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
