6c9a565fc34137154ce8073c17f9b898637f104f
Shane Park <shanejpark@gmail.com>
2023-12-09 16:17:42 -0500

    changed to es module

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: true,
-      },
-    ];
-  },
-};
 
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.nba.com",
-        port: "",
-      },
-    ],
-    domains: ["placehold.co"],
-  },
+export const images = {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "cdn.nba.com",
+      port: "",
+    },
+  ],
+  domains: ["placehold.co"],
 };
