b3a585a4eea88f6c19f838e9fa08a5472af118e9
Shane Park <shanejpark@gmail.com>
2023-12-14 03:30:40 -0500

    fixed country

--- src/app/(interface)/components/search/country.tsx
+++ src/app/(interface)/components/search/country.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
-import AsyncSelect, { useAsync } from "react-select/async";
-import { useForm } from "react-hook-form";
+import AsyncSelect from "react-select/async";
 
 export default function Country() {
   const [selectedCountry, setSelectedCountry] = useState("");
@@ -9,19 +8,21 @@ export default function Country() {
 
   async function promiseOptionsCountry(inputValue: String) {
     const url = "https://basketball-highlights-api.p.rapidapi.com/countries";
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "d3a5b34ba8msh9ac7e9cbb4859dfp11808djsncf2e1658c256",
-        "X-RapidAPI-Host": "basketball-highlights-api.p.rapidapi.com",
-      },
-    };
     try {
-      const response = await fetch(url, options);
+      const response = await fetch(url, {
+        method: "GET",
+        cache: "force-cache",
+        headers: {
+          "X-RapidAPI-Key":
+            "aa1fff7c82msh9d775d577faf9b8p16bed0jsne2743caa5c6a",
+          "X-RapidAPI-Host": "basketball-highlights-api.p.rapidapi.com",
+        },
+      });
       const result = await response.json();
-      const names = result.data.map(function (item: any) {
+      const names = result.map(function (item: any) {
         return { label: item["name"], value: item["name"] };
       });
+      console.log("wow");
       return names.filter((i: any) =>
         i.label.toLowerCase().includes(inputValue.toLowerCase())
       );
@@ -31,19 +32,21 @@ export default function Country() {
   }
 
   async function promiseOptionsLeague(inputValue: String) {
-    const url = `https://basketball-highlights-api.p.rapidapi.com/leagues?countryName=${selectedCountry}`;
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "d3a5b34ba8msh9ac7e9cbb4859dfp11808djsncf2e1658c256",
-        "X-RapidAPI-Host": "basketball-highlights-api.p.rapidapi.com",
-      },
-    };
-
+    const url = `https://basketball-highlights-api.p.rapidapi.com/countryName=${selectedCountry}`;
     try {
-      const response = await fetch(url, options);
+      const response = await fetch(url, {
+        method: "GET",
+        cache: "force-cache",
+        headers: {
+          "X-RapidAPI-Key":
+            "aa1fff7c82msh9d775d577faf9b8p16bed0jsne2743caa5c6a",
+          "X-RapidAPI-Host": "basketball-highlights-api.p.rapidapi.com",
+        },
+      });
+      console.log(url);
       const result = await response.json();
-      const names = result.map(function (item: any) {
+      console.log(result);
+      const names = result.data.map(function (item: any) {
         return { label: item["name"], value: item["name"] };
       });
       return names.filter((i: any) =>
